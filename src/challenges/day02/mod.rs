e3899a22db259990c1e08dcfc3905a6727899786
beowulf <61982523+beowolx@users.noreply.github.com>
2023-12-04 09:52:30 +0100

    day2: part 1 - inline functions that solve part 1 for better readability

--- src/challenges/day02/mod.rs
+++ src/challenges/day02/mod.rs
@@ -23,47 +23,40 @@ pub fn run() -> ((u32, f64), (u32, f64)) {
 const LIMITS: [(usize, u32); 3] = [(0, 12), (1, 13), (2, 14)];
 
 fn part1() -> u32 {
-  INPUT.lines().filter_map(process_line).sum()
-}
+  INPUT
+    .lines()
+    .filter_map(|line| {
+      // Splitting the line into game ID and colors
+      let (game_id_str, colors) = line.split_once(": ")?;
+      let game_id: u32 = game_id_str.split_whitespace().last()?.parse().ok()?;
+      // Iterating over each set of colors in the game
+      for set in colors.split(';') {
+        // Array to store the counts of each color
+        let mut counts = [0; 3];
 
-fn process_line(line: &str) -> Option<u32> {
-  let (game_id_str, colors) = line.split_once(": ")?;
-  let game_id: u32 = game_id_str.split_whitespace().last()?.parse().ok()?;
+        // Processing each color count in the set
+        for color in set.split(',').map(str::trim) {
+          let mut parts = color.split_whitespace();
+          let count: u32 = parts.next().unwrap().parse().unwrap_or(0);
+          let color_index = match parts.next().unwrap() {
+            "red" => 0,
+            "green" => 1,
+            "blue" => 2,
+            _ => return None,
+          };
+          counts[color_index] += count;
+        }
 
-  let mut counts = [0; 3];
-  if check_game_feasibility(colors, &mut counts) {
-    Some(game_id)
-  } else {
-    None
-  }
-}
+        // Check if the current set exceeds the limits
+        if !LIMITS.iter().all(|&(idx, limit)| counts[idx] <= limit) {
+          return None;
+        }
+      }
 
-fn check_game_feasibility(colors: &str, counts: &mut [u32; 3]) -> bool {
-  for set in colors.split(';') {
-    // Reset counts for each set to avoid allocate a new array
-    *counts = [0; 3];
-
-    for color in set.split(',').map(str::trim) {
-      let mut parts = color.split_whitespace();
-      let count: u32 = parts.next().unwrap().parse().unwrap_or(0);
-      let color_index = match parts.next().unwrap() {
-        "red" => 0,
-        "green" => 1,
-        "blue" => 2,
-        _ => return false,
-      };
-      counts[color_index] += count;
-    }
-
-    if !is_below_threshold(counts) {
-      return false;
-    }
-  }
-  true
-}
-
-fn is_below_threshold(counts: &[u32; 3]) -> bool {
-  LIMITS.iter().all(|&(idx, limit)| counts[idx] <= limit)
+      // Returning the game ID if all sets are within the limits
+      Some(game_id)
+    })
+    .sum()
 }
 
 fn part2() -> u32 {
